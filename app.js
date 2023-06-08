9562568c34d7ee9c7f621c4e477b44a88c0e29f4
Jose Armando Felix Zaragoza <josearmando.zara@gmail.com>
2023-06-08 12:37:18 -0700

    Minor changes for prismic integration

--- app.js
+++ app.js
@@ -10,15 +10,15 @@ app.get('/', (req, res) => {
     res.render('pages/home')
 })
 
-app.get('/collections', (req, res) => {
-    res.render('pages/collections')
+app.get('/collection', (req, res) => {
+    res.render('pages/collection')
 })
 
 app.get('/about', (req, res) => {
     res.render('pages/about')
 })
 
-app.get('/details/:id', (req, res) => {
+app.get('/details/:uid', (req, res) => {
     res.render('pages/details')
 })
 
