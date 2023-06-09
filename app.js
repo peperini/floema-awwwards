f92026e04b438db2e54649f3df582ccdf1ed9433
Jose Armando Felix Zaragoza <josearmando.zara@gmail.com>
2023-06-09 12:40:33 -0700

    Added api integration with Prismic repo and updated node_modules

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const path = require('path')
 const port = 3000
+
+const Prismic = require('@prismicio/client')
+const PrismicDOM = require('prismic-dom')
+
+const initApi = req => {
+    return Prismic.getApi(process.env.PRISMIC_ENDPOINT, {
+        accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+        req
+    })
+}
+
+const handleLinkResolver = doc => {
+    return '/'
+}
+
+app.use((req, res, next) => {
+    res.locals.ctx = {
+        endpoint: process.env.PRISMIC_ENDPOINT,
+        linkResolver: handleLinkResolver
+    }
+
+    res.locals.PrismicDOM = PrismicDOM
+
+    next()
+})
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
@@ -15,7 +42,15 @@ app.get('/collection', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-    res.render('pages/about')
+    initApi(req).then(api => {
+        api.query(
+            Prismic.Predicates.at('document.type', 'about')
+        ).then(response => {
+            console.log(response)
+
+            res.render('pages/about')
+        })
+    })
 })
 
 app.get('/details/:uid', (req, res) => {
