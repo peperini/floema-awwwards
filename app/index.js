081fafa4a688e8d93f3b5e5128e9426960aa61d7
Jose Armando Felix Zaragoza <josearmando.zara@gmail.com>
2023-07-18 18:26:01 -0700

    Added some new folers for the js structure

---
+++ app/index.js
@@ -0,0 +1,23 @@
+import About from 'pages/About'
+import Collections from 'pages/Collections'
+import Detail from 'pages/Detail'
+import Home from 'pages/Home'
+
+class App {
+    constructor () {
+        console.log('App')
+    }
+
+    createPages () {
+        this.pages = {
+            about: new About(),
+            collections: new Collections(),
+            detail: new Detail(),
+            home: new Home()
+        }
+
+        console.log(this.pages)
+    }
+}
+
+new App()
