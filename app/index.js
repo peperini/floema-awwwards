6bd8ece8b99be7019441322dbb0873d269822966
Jose Armando Felix Zaragoza <josearmando.zara@gmail.com>
2023-07-20 15:49:03 -0700

    Structured components and implemented page transitions and preloading transition

--- app/index.js
+++ app/index.js
@@ -1,12 +1,23 @@
+import each from 'lodash/each'
+
 import About from 'pages/About'
 import Collections from 'pages/Collections'
 import Detail from 'pages/Detail'
 import Home from 'pages/Home'
+import Preloader from 'components/Preloader'
 
 class App {
     constructor () {
+        this.createPreloader()
         this.createContent()
         this.createPages()
+
+        this.addLinkListeners()
+    }
+
+    createPreloader () {
+        this.preloader = new Preloader()
+        this.preloader.once('completed', this.onPreloaded.bind(this))
     }
 
     createContent () {
@@ -24,7 +35,54 @@ class App {
 
         this.page = this.pages[this.template]
         this.page.create()
+    }
+
+    onPreloaded () {
+        this.preloader.destroy()
+
         this.page.show()
+    }
+
+    async onChange (url) {
+        await this.page.hide()
+
+        const request = await window.fetch(url)
+
+        if (request.status === 200) {
+            const html = await request.text()
+            const div = document.createElement('div')
+
+            div.innerHTML = html
+
+            const divContent = div.querySelector('.content')
+            
+            this.template = divContent.getAttribute('data-template')
+
+            this.content.setAttribute('data-template', this.template)
+            this.content.innerHTML = divContent.innerHTML
+
+            this.page = this.pages[this.template]
+            this.page.create()
+            this.page.show()
+
+            this.addLinkListeners()
+        } else {
+            console.log('Error')
+        }
+    }
+
+    addLinkListeners () {
+        const links = document.querySelectorAll('a')
+
+        each(links, link => {
+            link.onclick = event => {
+                event.preventDefault()
+
+                const { href } = link
+                
+                this.onChange(href)
+            }
+        })
     }
 }
 
